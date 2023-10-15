d9a8eef2ba93677c0bd8b1088e11b5a0c15ac40e
Daniel López Guimaraes <danielectra@outlook.com>
2023-10-15 12:19:38 +0100

    Update revision number
    The revision number has increased on a recent Pou update 1.4.114

--- pou/online/client.py
+++ pou/online/client.py
@@ -22,7 +22,7 @@ class PouClient:
 		self.api = 1
 		self.c = 1
 		self.version = 4
-		self.revision = 256
+		self.revision = 263
 		self.host = "http://app.pou.me"
 		self.session = requests.Session()
 
